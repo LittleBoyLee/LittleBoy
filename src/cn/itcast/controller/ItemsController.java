a485239ed44ab46fb25500d67096d7b3cd105f0b
boy2194@163.com <boy2194@163.com>
2018-08-08 09:45:25 +0800

    massge

---
+++ src/cn/itcast/controller/ItemsController.java
@@ -0,0 +1,16 @@
+package cn.itcast.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/items")
+public class ItemsController {
+
+	@RequestMapping("list")
+	public String list(){
+		System.out.println("∑√Œ ≥…π¶");
+		return "success";
+	}
+	
+}
